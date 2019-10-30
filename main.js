fd15a117d08bca7463b4db8f413adef41548cf1d
Simone <simoneugazio1993@gmail.com>
2019-10-30 16:01:07 +0100

    3 push

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 //Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 //- Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo ////nell’ordine: nome, cognome e età.
 
+
+
 //creo un oggetto che descriva uno studente
 
 var studente = {
@@ -19,8 +21,46 @@ for (var key in studente) {
 
 //creo array di oggetti studente
 
-var studenti = {
-  "nomi" : ["Bart" , "Nelson" , "Lisa" , "Milause"],
-  "cognomi" : ["Simpson" , "Mandela" , "Simpson2" , "Nonricordo"],
-  "età" : ["13" , "15" , "18" , "22"]
-}
+var studenti = [
+      {
+          "nome" : "Bart",
+          "cognome": "Simpson",
+          "eta": 13
+
+      },
+      {
+          "nome": "Lisa",
+          "cognome": "Simpson2",
+          "eta": 14
+
+      },
+      {
+          "nome": "Nelson",
+          "cognome": "Mandela",
+          "eta": 15
+
+      }
+     ]
+
+
+
+  for (var i = 0; i < studenti.length; i++) {
+
+      console.log(studenti[i].nome + " " +  studenti[i].cognome);
+
+
+
+  }
+//- Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo ////nell’ordine: nome, cognome e età
+
+  var neostudente = {}
+
+  var nomeInput = prompt("inserisci il nome dell'utente");
+  var cognomeInput = prompt("inserisci il cognome dell'utente");
+  var etaInput = prompt("inserisci l'età dell'utente");
+
+  neostudente.nome= nomeInput;
+  neostudente.cognome= cognomeInput;
+  neostudente.età= etaInput;
+
+  console.log("lo studente neofita è:" , neostudente);
